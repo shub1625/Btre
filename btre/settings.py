d1e18300cc0fb29a252876305e2070e254cdf045
shubham.patne <shubham.patne1625@gmail.com>
2020-12-06 19:42:16 +0530

    local settings

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_HOST_USER=''
 EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
